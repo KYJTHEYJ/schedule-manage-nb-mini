11356f633f2c8458b8a53b2ca5a537477c0e94b4
KYJ <nananan1213@gmail.com>
2025-12-30 18:25:35 +0900

    fix: test 메서드 삭제

--- src/main/java/kyj/schedule_manage/service/CommentService.java
+++ src/main/java/kyj/schedule_manage/service/CommentService.java
@@ -3,10 +3,10 @@ package kyj.schedule_manage.service;
 import kyj.schedule_manage.dto.CreateCommentRequest;
 import kyj.schedule_manage.dto.CreateCommentResponse;
 import kyj.schedule_manage.entity.Comment;
+import kyj.schedule_manage.exception.NotFoundDataException;
 import kyj.schedule_manage.repository.CommentRepository;
 import kyj.schedule_manage.repository.ScheduleRepository;
 import lombok.RequiredArgsConstructor;
-import org.apache.coyote.BadRequestException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,7 +16,6 @@ public class CommentService {
     private final ScheduleRepository scheduleRepository;
 
     private void checkCommentContentLength(String content) {
-        content = "a".repeat(300);
         if(content.length() > 100) {
             throw new IllegalArgumentException("내용은 100자 까지 작성 가능합니다");
         }
@@ -44,7 +43,7 @@ public class CommentService {
 
     public CreateCommentResponse createComment(long scheduleId, CreateCommentRequest request) {
         if(!scheduleRepository.existsById(scheduleId)) {
-            throw new IllegalArgumentException("댓글을 입력 할 일정이 없습니다");
+            throw new NotFoundDataException("댓글을 입력할 일정이 없습니다");
         }
 
         checkCommentCount(scheduleId);
