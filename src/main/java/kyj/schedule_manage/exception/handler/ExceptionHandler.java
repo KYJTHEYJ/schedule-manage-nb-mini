986a6dba03f48507f5f50acfdd1a6f0b4fc4914b
KYJ <nananan1213@gmail.com>
2026-01-06 11:57:34 +0900

    refactor : 클래스 이름 수정

--- src/main/java/kyj/schedule_manage/exception/handler/ExceptionHandler.java
+++ src/main/java/kyj/schedule_manage/exception/handler/ExceptionHandler.java
@@ -5,32 +5,31 @@ import kyj.schedule_manage.exception.ErrorType;
 import kyj.schedule_manage.exception.NotFoundDataException;
 import kyj.schedule_manage.exception.NotMatchedException;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-public class exceptionHandler {
+public class ExceptionHandler {
 
-    @ExceptionHandler(NotFoundDataException.class)
+    @org.springframework.web.bind.annotation.ExceptionHandler(NotFoundDataException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse handleNotFoundException(NotFoundDataException e) {
         return new ErrorResponse(ErrorType.NOT_FOUND_DATA, e.getMessage());
     }
 
-    @ExceptionHandler(IllegalArgumentException.class)
+    @org.springframework.web.bind.annotation.ExceptionHandler(IllegalArgumentException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleIllegalArgumentException(IllegalArgumentException e) {
         return new ErrorResponse(ErrorType.INPUT_DATA_WRONG, e.getMessage());
     }
 
-    @ExceptionHandler(NotMatchedException.class)
+    @org.springframework.web.bind.annotation.ExceptionHandler(NotMatchedException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleNotMatchedException(NotMatchedException e) {
         return new ErrorResponse(ErrorType.NOT_MATCH, e.getMessage());
     }
 
-    @ExceptionHandler(Exception.class)
+    @org.springframework.web.bind.annotation.ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorResponse handleException(Exception e) {
         return new ErrorResponse(ErrorType.UNCONTROLLED_ERROR, "서버 오류가 발생하였습니다");
